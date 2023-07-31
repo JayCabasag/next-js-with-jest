8e5549a71ef2701f687d23c47fcfabdf251f26a7
Jay Cabasag <jaycabasag1999@gmail.com>
2023-07-31 22:47:04 +0800

    First commit

---
+++ __tests__/homepage.test.tsx
@@ -0,0 +1,31 @@
+import Home from '@/app/page';
+import { render, screen } from '@testing-library/react'
+
+describe('Home Page - Rendering', () => {
+    it('should have home page text', () => {
+        render(<Home />);
+        expect(screen.getByText('Home page')).toBeInTheDocument()
+    });
+
+    it('should have button with text Click Me', () => {
+        render(<Home />);
+        expect(screen.getByRole('button', { name: 'Click Me'}))
+        .toBeInTheDocument()
+    })
+
+    it('should have input field with label Enter Random Text', () => {
+        render (<Home />);
+        expect(screen.getByLabelText(/Enter random Text/)).toBeInTheDocument()
+    })
+
+    it('should have input field with label Enter Specific Text', () => {
+        render (<Home />);
+        expect(screen.getByLabelText(/specific/)).toBeInTheDocument()
+    })
+
+    it('should find input failed by placeholder text Search ', () => {
+        render (<Home />);
+        expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument()
+    })
+
+});
