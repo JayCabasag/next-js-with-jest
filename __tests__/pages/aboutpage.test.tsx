a029f7ff54b7387d7dba28198aa767fcab3d6997
Jay Cabasag <jaycabasag1999@gmail.com>
2023-08-02 23:32:50 +0800

    Added fix

--- __tests__/pages/aboutpage.test.tsx
+++ __tests__/pages/aboutpage.test.tsx
@@ -4,6 +4,10 @@ import { screen, render } from '@testing-library/react'
 describe('About page rendering', () => {
     it('shout display', () => {
         render(<AboutPage />);
-        expect( screen.getByText('About page')).toBeInTheDocument()
+        expect(screen.getByText('About page')).toBeInTheDocument()
+    })
+    it('should not find Element with text: This is the text!', () => {
+        render(<AboutPage />)
+        expect(screen.queryByText('This is the text!')).not.toBeInTheDocument();
     })
 })
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react'
 
 export default function AboutPage() {
+
+  const [showText, setShowText] = useState(false)
+
   return (
-    <div>
+    <div className='flex flex-col gap-2'>
         <h1>About page</h1>
+        {showText && <span>This is the text!</span>}
+        <button
+          className='bg-blue-500 max-w-max px-4 py-2 text-white rounded-md'
+          onClick={() => setShowText(!showText)}
+        >
+          Show
+        </button>
     </div>
   )
 }
